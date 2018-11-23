f438674aa1946cd09831cda3a5b0449d96b3712c
LiHexiang <lihexiang2012@163.com>
2018-11-23 09:49:04 +0800

    我的Demo添加bugly升级服务--1.1——1版本

--- app/src/main/java/com/example/lihexiang/mydemo/adapter/FlowlayotuAdapter.java
+++ app/src/main/java/com/example/lihexiang/mydemo/adapter/FlowlayotuAdapter.java
@@ -3,13 +3,8 @@ package com.example.lihexiang.mydemo.adapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.lihexiang.mydemo.R;
-import com.zhy.view.flowlayout.FlowLayout;
-import com.zhy.view.flowlayout.TagAdapter;
-import com.zhy.view.flowlayout.TagFlowLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,21 +29,7 @@ public class FlowlayotuAdapter extends RecyclerView.Adapter<FlowlayotuAdapter.Vi
         strs.add("标签1");
         strs.add("标签2");
         strs.add("标签3");
-        holder.flowLayout.setAdapter(new TagAdapter<String>(strs) {
-            @Override
-            public View getView(FlowLayout parent, int position, String str) {
-                TextView tv = new TextView(holder.itemView.getContext());
-                tv.setText(str);
-                return tv;
-            }
-        });
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(holder.itemView.getContext(), "点击了item", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     @Override
@@ -58,11 +39,9 @@ public class FlowlayotuAdapter extends RecyclerView.Adapter<FlowlayotuAdapter.Vi
 
     static class ViewHolder extends RecyclerView.ViewHolder {
 
-        TagFlowLayout flowLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            flowLayout = (TagFlowLayout) itemView.findViewById(R.id.flowLayout);
         }
     }
 }
